8745113a2e302feed3cb67092501cc146da1f7a2
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-11 18:20:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards( namesArray, event) {
+    let thankYouCards = []
+    for(let i=0; i<namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return thankYouCards
+}
+writeCards(["Brandon","Kashmir","Phyl"],"graduation");
+
+function countDown(number) {
+    while (number>0) {
+        console.log(number)
+        number -= 1;
+
+    }
+    console.log(number);
+}
